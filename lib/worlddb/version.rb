85b7e89ba973d6436fc561d3f4fc03ccd53502b2
Gerald Bauer <gerald.bauer@gmail.com>
2013-05-12 16:10:51 +0200

    prep 1.6.5

--- lib/worlddb/version.rb
+++ lib/worlddb/version.rb
@@ -1,6 +1,6 @@
 
 module WorldDb
-  VERSION = '1.6.4'  # sync version w/ sport.db - why? why not?
+  VERSION = '1.6.5'  # sync version w/ sport.db - why? why not?
 end
 
 ###########################################
