2601a9994ab6d15e03056ed7593281e93ee07f46
Gerald Bauer <gerald.bauer@gmail.com>
2013-05-12 14:22:14 +0200

    cleanup reader

--- lib/worlddb/models/city.rb
+++ lib/worlddb/models/city.rb
@@ -3,6 +3,14 @@
 module WorldDb::Models
 
 class City < ActiveRecord::Base
+  
+  extend WorldDb::TagHelper  # will add self.find_tags, self.find_tags_in_hash!, etc.
+
+  # NB: use extend - is_<type>? become class methods e.g. self.is_<type>? for use in
+  #   self.create_or_update_from_values
+  extend TextUtils::ValueHelper  # e.g. is_year?, is_region?, is_address?, is_taglist? etc.
+
+  
   self.table_name = 'cities'
 
   belongs_to :country, :class_name => 'Country', :foreign_key => 'country_id'
@@ -108,6 +116,152 @@ class City < ActiveRecord::Base
       City.create!( attr )
     end # each city
   end
+
+
+  def self.create_or_update_from_titles( titles, more_attributes = {} )  # ary of titles e.g. ['Wien', 'Graz'] etc.
+
+    # fix: add/configure logger for ActiveRecord!!!
+    logger = LogKernel::Logger.root
+
+    titles.each do |title|
+
+      new_attributes = {}
+      key = TextUtils.title_to_key( title )   # auto generate key from title
+
+      # check if it exists
+      # todo/fix: add country_id for lookup?
+      city = City.find_by_key( key )
+      if city.present?
+        logger.debug "update city #{city.id}-#{city.key}:"
+      else
+        logger.debug "create city:"
+        city = City.new
+        new_attributes[ :key ] = key
+      end
+        
+      new_attributes[ :title ] = title
+     
+      ## merge in passed in attribes (e.g. country_id etc.)
+      new_attributes.merge!( more_attributes )
+      
+      logger.debug new_attributes.to_json
+   
+      city.update_attributes!( new_attributes )
+      
+      ### todo/fix: add captial ref to country/region
+    end # each city
+  end  # method create_or_update_from_titles
+
+
+  def self.create_or_update_from_values( new_attributes, values, opts={} )
+
+    ## opts e.g. :skip_tags true|false
+
+    ## fix: add/configure logger for ActiveRecord!!!
+    logger = LogKernel::Logger.root
+
+    value_numbers     = []
+    value_tag_keys    = []
+      
+    ### check for "default" tags - that is, if present new_attributes[:tags] remove from hash
+    value_tag_keys += find_tags_in_hash!( new_attributes )
+
+    new_attributes[ :c ] = true   # assume city type by default (use metro,district to change in fixture)
+
+    ## check for optional values
+    values.each_with_index do |value,index|
+      if value =~ /^region:/   ## region:
+        value_region_key = value[7..-1]  ## cut off region: prefix
+        ## NB: requires country_id to make unique!
+        value_region = Region.find_by_key_and_country_id!( value_region_key, new_attributes[:country_id] )
+        new_attributes[ :region_id ] = value_region.id
+      elsif value =~ /^metro$/   ## metro(politan area)
+        new_attributes[ :c ] = false   # turn off default c|city flag; make it m|metro only
+        new_attributes[ :m ] = true    
+      elsif value =~ /^country:/   ## country:
+        value_country_key = value[8..-1]  ## cut off country: prefix
+        value_country = Country.find_by_key!( value_country_key )
+        new_attributes[ :country_id ] = value_country.id
+      elsif value =~ /^metro:/   ## metro:
+        value_city_key = value[6..-1]  ## cut off metro: prefix
+        value_city = City.find_by_key!( value_city_key )
+        new_attributes[ :city_id ] = value_city.id
+      elsif value =~ /^city:/   ## city:
+        value_city_key = value[5..-1]  ## cut off city: prefix
+        value_city = City.find_by_key!( value_city_key )
+        new_attributes[ :city_id ] = value_city.id
+        new_attributes[ :c ] = false # turn off default c|city flag; make it d|district only
+        new_attributes[ :d ] = true  
+      elsif value =~ /^m:/   ## m:
+        value_popm_str = value[2..-1]  ## cut off m: prefix
+        value_popm = value_popm_str.gsub(/[ _]/, '').to_i
+        new_attributes[ :popm ] = value_popm
+        new_attributes[ :m ] = true   #  auto-mark city as m|metro too
+      elsif is_region?( value )  ## assume region code e.g. TX for city
+        value_region = Region.find_by_key_and_country_id!( value.downcase, new_attributes[:country_id] )
+        new_attributes[ :region_id ] = value_region.id
+      elsif value =~ /^[A-Z]{2,3}$/  ## assume two or three-letter code
+        new_attributes[ :code ] = value
+      elsif value =~ /^([0-9][0-9 _]+[0-9]|[0-9]{1,2})(?:\s*(?:km2|km²)\s*)$/
+        ## allow numbers like 453 km²
+        value_numbers << value.gsub( 'km2', '').gsub( 'km²', '' ).gsub(/[ _]/, '').to_i
+      elsif value =~ /^([0-9][0-9 _]+[0-9])|([0-9]{1,2})$/    ## numeric (nb: can use any _ or spaces inside digits e.g. 1_000_000 or 1 000 000)
+        value_numbers << value.gsub(/[ _]/, '').to_i
+      elsif (values.size==(index+1)) && is_taglist?( value )   # tags must be last entry
+        logger.debug "   found tags: >>#{value}<<"
+        value_tag_keys += find_tags( value )
+      else
+        # issue warning: unknown type for value
+        logger.warn "unknown type for value >#{value}<"
+      end
+    end # each value
+
+    if value_numbers.size > 0
+      new_attributes[ :pop  ] = value_numbers[0]   # assume first number is pop for cities
+      new_attributes[ :area ] = value_numbers[1]  
+    end  # if value_numbers.size > 0
+
+    rec = City.find_by_key( new_attributes[ :key ] )
+
+    if rec.present?
+      logger.debug "update City #{rec.id}-#{rec.key}:"
+    else
+      logger.debug "create City:"
+      rec = City.new
+    end
+
+    logger.debug new_attributes.to_json
+
+    rec.update_attributes!( new_attributes )
+
+      ##################
+      ## add taggings
+
+      ## todo/fix: reuse - move add taggings into method etc.
+
+      if value_tag_keys.size > 0
+
+        if opts[:skip_tags].present?
+          logger.debug "   skipping add taggings (flag skip_tag)"
+        else
+          value_tag_keys.uniq!  # remove duplicates
+          logger.debug "   adding #{value_tag_keys.size} taggings: >>#{value_tag_keys.join('|')}<<..."
+
+          ### fix/todo: check tag_ids and only update diff (add/remove ids)
+
+          value_tag_keys.each do |key|
+            tag = Tag.find_by_key( key )
+            if tag.nil?  # create tag if it doesn't exit
+              logger.debug "   creating tag >#{key}<"
+              tag = Tag.create!( key: key )
+            end
+            rec.tags << tag
+          end
+        end
+      end
+
+    rec
+  end # method create_or_update_from_values
 
 end # class Cities
 
--- lib/worlddb/models/region.rb
+++ lib/worlddb/models/region.rb
@@ -2,6 +2,13 @@ module WorldDb::Models
 
 class Region < ActiveRecord::Base
 
+  extend WorldDb::TagHelper  # will add self.find_tags, self.find_tags_in_hash!, etc.
+
+  # NB: use extend - is_<type>? become class methods e.g. self.is_<type>? for use in
+  #   self.create_or_update_from_values
+  extend TextUtils::ValueHelper  # e.g. is_year?, is_region?, is_address?, is_taglist? etc.
+
+
   belongs_to :country, :class_name => 'Country', :foreign_key => 'country_id'
 
   has_many :cities, :class_name => 'City', :foreign_key => 'region_id'
@@ -9,7 +16,7 @@ class Region < ActiveRecord::Base
   has_many :taggings, :as => :taggable
   has_many :tags,     :through => :taggings
 
-  validates :key, :format => { :with => /^[a-z]{2,}$/, :message => 'expected two or more lowercase letters a-z' }
+  validates :key, :format => { :with => /^[a-z]+$/, :message => 'expected one or more lowercase letters a-z' }
   validates :code, :format => { :with => /^[A-Z_]{2,3}$/, :message => 'expected two or three uppercase letters A-Z (and _)' }, :allow_nil => true
 
 
@@ -47,6 +54,109 @@ class Region < ActiveRecord::Base
       Region.create!( attr )
     end # each region
   end
+
+
+  def self.create_or_update_from_values( new_attributes, values, opts={} )
+
+    ## opts e.g. :skip_tags true|false
+
+    ## fix: add/configure logger for ActiveRecord!!!
+    logger = LogKernel::Logger.root
+
+    value_numbers     = []
+    value_tag_keys    = []
+    value_cities      = []
+
+    ### check for "default" tags - that is, if present new_attributes[:tags] remove from hash
+    value_tag_keys += find_tags_in_hash!( new_attributes )
+
+    ## check for optional values
+    values.each_with_index do |value,index|
+      if value =~ /^country:/   ## country:
+        value_country_key = value[8..-1]  ## cut off country: prefix
+        value_country = Country.find_by_key!( value_country_key )
+        new_attributes[ :country_id ] = value_country.id
+      elsif value =~ /^[A-Z]{2,3}$/  ## assume two or three-letter code
+        new_attributes[ :code ] = value
+      elsif value =~ /^([0-9][0-9 _]+[0-9]|[0-9]{1,2})(?:\s*(?:km2|km²)\s*)$/
+        ## allow numbers like 453 km²
+        value_numbers << value.gsub( 'km2', '').gsub( 'km²', '' ).gsub(/[ _]/, '').to_i
+      elsif value =~ /^([0-9][0-9 _]+[0-9])|([0-9]{1,2})$/    ## numeric (nb: can use any _ or spaces inside digits e.g. 1_000_000 or 1 000 000)
+        value_numbers << value.gsub(/[ _]/, '').to_i
+      elsif (values.size==(index+1)) && is_taglist?( value )   # tags must be last entry
+        logger.debug "   found tags: >>#{value}<<"
+        value_tag_keys += find_tags( value )
+      else
+          
+        ### assume it is the capital city - mark it for auto add
+        value_cities << value
+        next
+   
+        # issue warning: unknown type for value
+        # logger.warn "unknown type for value >#{value}<"
+      end
+    end # each value
+
+    if value_numbers.size > 0
+      new_attributes[ :area ] = value_numbers[0]
+      new_attributes[ :pop  ] = value_numbers[1]
+    end  # if value_numbers.size > 0
+
+    ## todo: assert that country_id is present/valid, that is, NOT null
+    rec = Region.find_by_key_and_country_id( new_attributes[ :key ], new_attributes[ :country_id] )
+
+    if rec.present?
+      logger.debug "update Region #{rec.id}-#{rec.key}:"
+    else
+      logger.debug "create Region:"
+      rec = Region.new
+    end
+      
+    logger.debug new_attributes.to_json
+   
+    rec.update_attributes!( new_attributes )
+
+    #################
+    # auto add capital cities
+
+    City.create_or_update_from_titles( value_cities,
+                                            region_id:  rec.id,
+                                            country_id: rec.country_id )
+
+    ### todo/fix: add captial ref to country/region
+
+
+    ##################
+    # add taggings 
+
+    ## todo/fix: reuse - move add taggings into method etc.
+
+      if value_tag_keys.size > 0
+        
+        if opts[:skip_tags].present?
+          logger.debug "   skipping add taggings (flag skip_tag)"
+        else
+          value_tag_keys.uniq!  # remove duplicates
+          logger.debug "   adding #{value_tag_keys.size} taggings: >>#{value_tag_keys.join('|')}<<..."
+
+          ### fix/todo: check tag_ids and only update diff (add/remove ids)
+
+          value_tag_keys.each do |key|
+            tag = Tag.find_by_key( key )
+            if tag.nil?  # create tag if it doesn't exit
+              logger.debug "   creating tag >#{key}<"
+              tag = Tag.create!( key: key )
+            end
+            rec.tags << tag
+          end
+        end
+      end
+
+    rec
+
+  end # method create_or_update_from_values
+
+
 
 end # class Region
 
