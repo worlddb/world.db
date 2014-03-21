b0766964f91fa3e6eee60fc524f8c735d40d68d8
Gerald Bauer <gerald.bauer@gmail.com>
2014-03-21 10:34:59 +0100

    cleanup attic

--- attic/loader.rb
+++ attic/loader.rb
@@ -1,29 +1,16 @@
-module WorldDB
+module WorldDb
 
 class Loader
 
-  def initialize( logger=nil )
-    if logger.nil?
-      @logger = Logger.new(STDOUT)
-      @logger.level = Logger::INFO
-    else
-      @logger = logger
-    end
-  end
-
-  attr_reader :logger
+  include LogUtils::Logging
 
 
   def run( opts, args )
 
     args.each do |arg|
       name = arg     # File.basename( arg, '.*' )
-      
-      if opts.load?
-        load_fixtures_builtin( name )
-      else
-        load_fixtures_with_include_path( name, opts.data_path )
-      end
+
+      load_fixtures_with_include_path( name, opts.data_path )
     end
     
   end # method run
@@ -32,7 +19,7 @@ class Loader
   class CodeContext
     ## make models available in worlddb module by default with namespace
     #  e.g. lets you use City instead of Models::City 
-    include WorldDB::Models
+    include WorldDb::Models
   end
 
   ### todo: rename to load_fixtures_w_include_path (a little shorter - why? why not?)
@@ -43,13 +30,13 @@ class Loader
 
     CodeReader.new( logger, path ).eval( CodeContext )
 
-    # Prop.create!( :key => "db.#{name}.version", :value => WorldDB::VERSION )    
+    # Prop.create!( :key => "db.#{name}.version", :value => WorldDb::VERSION )    
   end
 
   def load_fixtures_builtin( name ) # load from gem (built-in)
-    load_fixtures_with_include_path( name, WorldDB.data_path )
+    load_fixtures_with_include_path( name, WorldDb.data_path )
   end
 
 
 end # class Loader
-end # module WorldDB
+end # module WorldDb
